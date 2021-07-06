4ce379972b1fe8e0792d57401171b077745cf9ae
Dobrosavljevic Luka <bdobrl@bbcag.ch>
2021-07-06 14:36:20 +0200

    Design

--- app/src/main/java/com/example/to_doliste/MainActivity.java
+++ app/src/main/java/com/example/to_doliste/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.to_doliste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        FloatingActionButton b = findViewById(R.id.floating_action_button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), createActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/to_doliste/updateActivity.java
+++ app/src/main/java/com/example/to_doliste/updateActivity.java
@@ -5,42 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class createActivity extends AppCompatActivity {
+public class updateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create);
+        setContentView(R.layout.activity_update);
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
-        setTitle("Aufgabe hinzufügen");
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
-        if(itemId == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        setTitle("Aufgabe bearbeiten");
     }
     @Override
     protected void onResume() {
         super.onResume();
-        Button speichern = findViewById(R.id.create_btnSpeichern);
+        Button speichern = findViewById(R.id.update_btnSpeichern);
         speichern.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent myIntent = new Intent(v.getContext(), MainActivity.class);
+                Intent myIntent = new Intent(v.getContext(), updateActivity.class);
                 startActivity(myIntent);
             }
         });
 
+        Button verwerfen = findViewById(R.id.update_btnVerwerfen);
+        verwerfen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), updateActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
